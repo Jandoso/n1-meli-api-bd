e4585baff383a2e3ec79db40bc67850bf04bf53d
jandoso <carol.jandoso@gmail.com>
2019-11-13 19:27:27 -0300

    atualização model alunas

--- src/model/alunas.js
+++ src/model/alunas.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
+//schema de alunas
+//representação das características do objeto aluna, que será utilizado no model
+
+//Ex. de tipos de valores: String (texto), Number (Numérico), Date (Data), Booleano (Boolean)
+
 const AlunasSchema = new mongoose.Schema({
     nome: { type: String },
-    dateOfBirth: { type: Date },
-    nasceuEmSp: { type: Boolean },
+    dateOfBirth: { type: String },
+    nasceuEmSp: { type: String },
     livros: [{
         titulo: String,
-        leu: Boolean
+        leu: String,
     }]
-});
+})
+
+
+const Alunas = mongoose.model('Alunas', AlunasSchema);
+
+module.exports = Alunas;
+
+//exemplo de schema com campos obrigatórios
+//var sampleSchema = new Schema({ name: { type: String, required: true } });
